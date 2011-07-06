89b4c456374fee2546b37f792470c704415df189
Callum Jones <callum@callumj.com>
2011-07-06 11:25:22 +0800

    Changing data structure, web service endpoint

--- server/web_app.rb
+++ server/web_app.rb
@@ -5,14 +5,15 @@ load "#{File.dirname(__FILE__)}/init.rb"
 get '/this_week/:tag.:format' do
   classifier = params[:tag]
   
-  stubs = Stub.where(:classifiers => classifier, :created_at.gte => 7.days.ago).all
+  stubs = Stub.where(:classifiers => classifier, :created_at.gte => 2.weeks.ago).all
+  doc = {:time => Time.now.to_i, :count => stubs.count,:data => stubs}
   if ("json".eql?(params[:format]))
     content_type "application/json"
-    {:time => Time.now.to_i, :data => stubs}.to_json
+    doc.to_json
   elsif ("xml".eql?(params[:format]))
     content_type "text/xml"
-    {:time => Time.now.to_i, :data => stubs}.to_xml
+    doc.to_xml
   else
-    "hi"
+    "ERROR"
   end
 end
