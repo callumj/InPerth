31b4be5e14a2a9b7941b858f1c9636bd28a0fb3e
Callum Jones <callum@callumj.com>
2011-07-06 12:22:01 +0800

    Adding all option

--- server/web_app.rb
+++ server/web_app.rb
@@ -8,7 +8,13 @@ get '/stub/:tag.:format' do
   search_date = 2.weeks.ago
   search_date = Time.at(params[:since]) if params[:since] != nil
   
-  stubs = Stub.where(:classifiers => classifier, :created_at.gte => search_date).sort(:created_at.desc).all
+  stubs = []
+  
+  if "all".eql?(classifier)
+    stubs = Stub.where(:created_at.gte => search_date).sort(:created_at.desc).all
+  else
+    stubs = Stub.where(:classifiers => classifier, :created_at.gte => search_date).sort(:created_at.desc).all
+  end
   doc = {:time => Time.now.to_i, :count => stubs.count,:data => stubs}
   if ("json".eql?(params[:format]))
     content_type "application/json"
