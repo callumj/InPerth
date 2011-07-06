e1817ae6820d566ecc0c9ae8b5eae93377784955
Callum Jones <callum@callumj.com>
2011-07-06 11:28:29 +0800

    Fixing

--- server/web_app.rb
+++ server/web_app.rb
@@ -2,17 +2,17 @@ require "sinatra"
 
 load "#{File.dirname(__FILE__)}/init.rb"
 
-get '/this_week/:tag.:format' do
+get '/stub/:tag.:format' do
   classifier = params[:tag]
   
-  stubs = Stub.where(:classifiers => classifier, :created_at.gte => 2.weeks.ago).all
+  search_date = 2.weeks.ago
+  search_date = Time.at(params[:since]) if params[:since] != nil
+  
+  stubs = Stub.where(:classifiers => classifier, :created_at.gte => search_date).all
   doc = {:time => Time.now.to_i, :count => stubs.count,:data => stubs}
   if ("json".eql?(params[:format]))
     content_type "application/json"
     doc.to_json
-  elsif ("xml".eql?(params[:format]))
-    content_type "text/xml"
-    doc.to_xml
   else
     "ERROR"
   end
