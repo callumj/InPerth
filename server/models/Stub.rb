c61292eede40344126dc4159dd6270065cabf56a
Callum Jones <callum@callumj.com>
2011-07-28 02:00:55 +0800

    Added entitlements Stub now has offline support

--- server/models/Stub.rb
+++ server/models/Stub.rb
@@ -10,6 +10,7 @@ class Stub
   key :tags, Array
   key :created_at, Time
   key :classifiers, Array
+  key :offline_archive, String
   
   def as_json(options={})
     {:key => self.id, :title => self.title, :uri => self.uri, :desc => self.description, :classifiers => self.classifiers, :tags => self.tags, :time => self.created_at.in_time_zone.to_s, :server_time => self.created_at.to_i, :provider => {:id => self.provider.id, :title => self.provider.title}}
