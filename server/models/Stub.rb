acf7f62c6ccf74ce0465058f4b625d8de5d729a3
Callum Jones <callum@callumj.com>
2011-07-31 00:02:07 +0800

    Fixing updated_at string rendering

--- server/models/Stub.rb
+++ server/models/Stub.rb
@@ -16,11 +16,11 @@ class Stub
   key :offline_archive, String
   
   def as_json(options={})
-    {:key => self.id, :title => self.title, :uri => self.uri, :desc => self.description, :classifiers => self.classifiers, :tags => self.tags, :time => self.created_at.in_time_zone.to_s, :server_time => self.created_at.to_i, :offline_archive => self.offline_archive, :updated_at => self.updated_at,:provider => {:id => self.provider.id, :title => self.provider.title}}
+    {:key => self.id, :title => self.title, :uri => self.uri, :desc => self.description, :classifiers => self.classifiers, :tags => self.tags, :time => self.created_at.in_time_zone.to_s, :server_time => self.created_at.to_i, :offline_archive => self.offline_archive, :updated_at => self.updated_at.in_time_zone.to_s ,:provider => {:id => self.provider.id, :title => self.provider.title}}
   end
   
   def as_xml(options={})
-    {:key => self.id, :title => self.title, :uri => self.uri, :desc => self.description, :classifiers => self.classifiers, :tags => self.tags, :time => self.created_at.in_time_zone.to_s, :server_time => self.created_at.to_i, :offline_archive => self.offline_archive, :updated_at => self.updated_at,:provider => {:id => self.provider.id, :title => self.provider.title}}
+    {:key => self.id, :title => self.title, :uri => self.uri, :desc => self.description, :classifiers => self.classifiers, :tags => self.tags, :time => self.created_at.in_time_zone.to_s, :server_time => self.created_at.to_i, :offline_archive => self.offline_archive, :updated_at => self.updated_at.in_time_zone.to_s ,:provider => {:id => self.provider.id, :title => self.provider.title}}
   end
   
   def update_time
