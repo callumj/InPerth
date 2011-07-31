4af39fecf26190bbe762e3b5b90f58a1d2b53024
Callum Jones <callum@callumj.com>
2011-08-01 00:24:56 +0800

    Basic web page

--- server/public/js/stubs.js
+++ server/public/js/stubs.js
@@ -5,13 +5,35 @@ function get_stubs_since(time, category) {
 		time = 0 ;
 		
 	query_param = time == 0 ? '' : '?since=' + time;
-	results = []
 	$.getJSON('/stub/' + category +'.json' + query_param, function(contents) {
-	  	results = contents.data;
+		newer_obj = null;
+	  	for (i = 0; i < contents.data.length; i++)
+		{
+			//check it already exists
+			stub = contents.data[i];
+			find = $("#stub_" + stub.key);
+			if (!(find.length))
+			{
+				new_div = $("<div>");
+				new_div.attr("id", $("#stub_" + stub.key));
+				new_div.attr("class", "stub " + stub.type + "-stub");
+				header = $("<h1>");
+				header.text(stub.title);
+				new_div.appender(header);
+				if (newer_obj == null)
+				{
+					$("#stubs").prepend(new_div);
+				}
+				else
+				{
+					new_div.insertAfter(newer_obj);
+				}
+				newer_obj = new_div;
+			}
+		}
 	});
-	return results;
 }
 
 $(function() {
-	get_stubs_since(null,null);
+	get_stubs_since(since_time,null);
 });
